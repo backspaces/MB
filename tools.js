b3420a3830f29aeab05a6a724eb30ade660a177a
backspaces <owen@backspaces.net>
2020-05-05 16:38:26 -0600

    Agent/MB first light: test.html - test.html:   - add hello model   - add view   - add features info to patches   - add World.bbox transform   - add geo coords to patches   - add turtle's current hover feature   - animate - tools.js: moved utils from test.html into separate file - bin/addFeatureID: use state + county fips rather than index - bin/addPopulation: new, merge population data into nmcounties.json - bin/redfish2pop: new, convert steve's csv to json - data/nmpopulation.json: json file made from redfish2pop - data/nmcounties.json: add population from above, use fips id - data/uscounties.json: use fips id

--- tools.js
+++ tools.js
@@ -1,5 +1,41 @@
-export function mapLoad(map) {
+export function mapLoadPromise(map) {
     return new Promise((resolve, reject) => {
         map.on('load', () => resolve())
     })
 }
+
+export async function importModel(url, width, bbox) {
+    const [west, south, east, north] = bbox
+    const aspect = (east - west) / (north - south)
+
+    const Model = (await import(url)).default
+    const worldOptions = {
+        minX: 0,
+        minY: 0,
+        maxX: width,
+        maxY: Math.round(width / aspect),
+    }
+    const model = new Model(worldOptions)
+    await model.startup()
+    model.setup()
+
+    util.toWindow({ Model, model, width, aspect, worldOptions })
+    return model
+}
+
+export function bboxCenter(bbox) {
+    const [west, south, east, north] = bbox
+    return [(west + east) / 2, (south + north) / 2]
+}
+
+export function bboxCoords(bbox) {
+    const [west, south, east, north] = bbox
+    return [
+        [west, north],
+        [east, north],
+        [east, south],
+        [west, south],
+    ]
+}
+
+export default { mapLoadPromise, importModel, bboxCenter, bboxCoords }
